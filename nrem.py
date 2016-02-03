1466aef4c405951a900ae9c220083e3f6e08ea6e
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-02-03 22:59:37 +0530

    Complete

--- nrem.py
+++ nrem.py
@@ -44,9 +44,14 @@ aoPair = inRDD.flatMap(lambda line: attr_key(line.split("\t")))
 	+----+---+
 	|attr|obj|
 	+----+---+
-	|   1|  x|
-	|   1|  y|
-	|   2|  x|
+	|   1|  a|
+	|   2|  a|
+	|   3|  a|
+	|   1|  b|
+	|   2|  b|
+	|   3|  b|
+	|   1|  c|
+	|   3|  c|
 	+----+---+
 """
 
@@ -64,15 +69,112 @@ window = Window.orderBy("attr").partitionBy("obj")
 	+----+---+----+
 	|attr|obj|prev|
 	+----+---+----+
-	|   1|  x|null|
-	|   2|  x|   1|
-	|   1|  y|null|
+	|   1|  a|null|
+	|   2|  a|   1|
+	|   3|  a|   2|
+	|   1|  b|null|
+	|   2|  b|   1|
+	|   3|  b|   2|
+	|   1|  c|null|
+	|   3|  c|   1|
 	+----+---+----+
+
 """
-memorize = aoDF.select("attr", "obj", lag("attr",1, None).over(window).alias("prev")).cache()
+memorize = aoDF.select("attr", "obj", lag("attr",1, None).over(window).alias("prev"))
 
 
+##Back to RDD
+#
+"""
+	DataFrame : [attr, obj, prev] -> RDD : [(attr, (obj, prev))]
+
+	[(1, (u'a', None)),
+	 (2, (u'a', 1)),
+	 (3, (u'a', 2)),
+	 (1, (u'b', None)),
+	 (2, (u'b', 1)),
+	 (3, (u'b', 2)),
+	 (1, (u'c', None)),
+	 (3, (u'c', 1))]
+
+"""
+mappedRDD = memorize.map(lambda row: (row.attr, (row.obj, row.prev)))
 
 
+#Group by 'attr' and collect tuple(obj, prev) into lists
+"""
+	[(1, [(u'a', None), (u'b', None), (u'c', None)]),
+	 (2, [(u'a', 1), (u'b', 1)]),
+	 (3, [(u'a', 2), (u'b', 2), (u'c', 1)])]
 
-"""++++++++++Incomplete+++++++++++"""
+"""
+groupedByAttr = mappedRDD.groupByKey().mapValues(list).cache()
+
+##Calling an action to materialize cache.
+
+groupedByAttr.take(5)
+
+##Function to collect matched and eliminated pairs
+
+def pairFilter(elim=False):
+	##Return correct function based on 'elim'
+
+	def matchedPair(l):
+		s = set()
+		#Two loops to iterate through list of obj, prev
+		#Outer loop
+		for obj_o, prev_o in l:
+			#Inner loop
+			for obj_i, prev_i in l:
+				#Case for matching
+				if obj_o != obj_i and ((prev_o != prev_i) or (not prev_i and not prev_o)):
+					#No duplicates
+					s.add("-".join(sorted([obj_o, obj_i])))
+		return s
+
+	def eliminatedPair(l):
+		s = set()
+		for obj_o, prev_o in l:
+			for obj_i, prev_i in l:
+				if obj_o != obj_i and ((prev_o == prev_i) and (prev_o and prev_i)):
+					s.add("-".join(sorted([obj_o, obj_i])))
+		return s
+
+	"""
+		>>> matched = pairFilter(elim=False)
+		>>> eliminated = pairFilter(elim=True)
+	"""	
+	if elim:
+		return eliminatedPair
+	else:
+		return matchedPair
+## matched is an instace of matchedPair()
+matched = pairFilter(elim=False)
+##eliminated is an instance of eliminatedPair()
+eliminated = pairFilter(elim=True)
+##RDD with all the matched pairs
+"""
+	***** Problem in the algorithm : if objects skip attributes then you get duplicate pairs across keys/attributes ****
+		*** Since 'a' and 'c', and 'b' an 'c' did not have common previous attributes. ***
+
+	[(1, {u'a-b', u'a-c', u'b-c'}), (3, {u'a-c', u'b-c'})]
+"""
+##Filter to eliminate empty sets
+matchedPairs = groupedByAttr.mapValues(lambda x: matched(x)).filter(lambda (x, y): True if len(y) else False)
+#RDD with all the eliminated pairs
+"""
+	****  'a' and 'b' both had common previous attributes ****
+	[(2, {u'a-b'}), (3, {u'a-b'})]
+"""
+##Filter to eliminate empty sets
+eliminatedPairs = groupedByAttr.mapValues(lambda x: elimated(x)).filter(lambda (x, y): True if len(y) else False)
+
+##Saving outputs
+
+matchedPairs.saveAsTextFile(sys.argv[2] + "/matched")
+
+eliminatedPairs.saveAsTextFile(sys.argv[1] + "/eliminated")
+
+sc.stop()
+
+## End of Program
